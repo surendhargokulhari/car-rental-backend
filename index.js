aa1dc8e7d289b526fd286aafa02e291a254bdfaa
gokulakrishnan <gokulhari182@gmail.com>
2024-10-04 19:27:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function bookCar(carModel) {
+    document.getElementById('cars').style.display = 'none';
+    document.getElementById('booking-form').style.display = 'block';
+    document.getElementById('car-model').value = carModel;
+}
+
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for booking with us!');
+    document.getElementById('booking-form').reset();
+    document.getElementById('booking-form').style.display = 'none';
+    document.getElementById('cars').style.display = 'block';
+});
+
+
+
+
