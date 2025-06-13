03aad9783b3de75401c85d4568ecf46b182e1692
gokulakrishnan <gokulhari182@gmail.com>
2025-06-13 10:32:47 +0530

    commit

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -4,7 +4,9 @@ const bookingSchema = new mongoose.Schema({
   name: String,
   email: String,
   carModel: String,
-  phone: String,
-}, { timestamps: true });
+  phone: String
+}, {
+  timestamps: true  // <-- this adds createdAt and updatedAt automatically
+});
 
-module.exports = mongoose.model("Booking", bookingSchema);
+module.exports = mongoose.model('Booking', bookingSchema);
